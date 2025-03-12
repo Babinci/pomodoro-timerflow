8bbd13a1ca0256fba0307e69f76ec4e1baacf452
Wojtek <walkowo@gmail.com>
2025-03-12 21:46:12 +0100

    progress

--- backend/db_migrate.sh
+++ backend/db_migrate.sh
@@ -17,11 +17,11 @@ if [ "$1" = "makemigrations" ]; then
         echo "Usage: $0 makemigrations \"your migration message\""
         exit 1
     fi
-    docker exec -it $CONTAINER_NAME python /app/manage_db.py makemigrations "$2"
+    docker exec -it $CONTAINER_NAME python /app/app/manage_db.py makemigrations "$2"
 elif [ "$1" = "migrate" ]; then
-    docker exec -it $CONTAINER_NAME python /app/manage_db.py migrate
+    docker exec -it $CONTAINER_NAME python /app/app/manage_db.py migrate
 elif [ "$1" = "showmigrations" ]; then
-    docker exec -it $CONTAINER_NAME python /app/manage_db.py showmigrations
+    docker exec -it $CONTAINER_NAME python /app/app/manage_db.py showmigrations
 else
     echo "Usage: $0 [makemigrations \"message\"|migrate|showmigrations]"
     exit 1
